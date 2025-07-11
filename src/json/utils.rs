ebb8a43817be1c4df172ae7b158480778ef27300
Alan <alan.silva_noesis@prestador.globo>
2025-07-11 20:16:27 -0300

    saving progress

--- src/json/utils.rs
+++ src/json/utils.rs
@@ -1,10 +1,21 @@
+use std::fs;
+
 use serde::{de::DeserializeOwned, Serialize};
 use serde_json::Value;
 
 pub fn serialize<T: Serialize>(object: &T) -> Value {
-    return serde_json::to_value(&object).unwrap();
+    return serde_json::to_value(&object).expect("Failed to serialize");
 }
 
 pub fn deserialize<T: DeserializeOwned>(object: Value) -> T {
     return serde_json::from_value(object).expect("Failed to deserialize");
+}
+
+pub fn parse_str(content: &str) -> Value {
+    return serde_json::from_str(content).expect("Failed to parse_str");
+}
+
+pub fn parse_file(path: &str) -> Value {
+    let content = fs::read_to_string(path).expect("Failed to parse_file");
+    return parse_str(&content);
 }
